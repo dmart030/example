59effc10b252f193cc7acd9710de76e2178721fd
dmart030 <dmart030@ucr.edu>
2015-04-10 13:51:48 -0700

    :a:

--- week01/Ex2.py
+++ week01/Ex2.py
@@ -36,8 +36,8 @@ while i < 8:
         g += 1
     i += 1
 w = round(g/8.,2)
-print"The number of failed students are: ",j, "\n"
 failed_grades = [round(n,2) for n in failed_grades]
+print"The number of failed students are: ",j, "\n"
 print"The grades of the failed students are:", failed_grades, "\n"
 
 print"The number of outstanding students are: ", g, "\n"
