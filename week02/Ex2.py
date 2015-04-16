dc4a642c76c5e545e2be0ccc01d3d8e82b5d748e
dmart030 <dmart030@ucr.edu>
2015-04-16 02:54:36 -0700

    :accept:

---
+++ week02/Ex2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 02:22:55 2015
+
+@author: Dominic Martinez-Ta
+"""
+'''
+2) Download from the github repository of the course 
+(https://github.com/lsales/rep_Phys177),the file called “velocities.txt” in the
+ “extra_info_labs” folder (just make “git pull” if you have already cloned the 
+ repository before). This file has 2 columns, time t in seconds (1st column), 
+ and the x-velocity velocity in meters persecond of a particle, measured once 
+ every second from time t=0 to t=100. Write a program to do the following: 
+ • Read in the data and calculate the approximate distance traveled by the 
+ particle in the x direction as a function of time. Use your functions to 
+ compute this using the trapezoidal rule and the Simpson’s rule. Print results 
+ to a file in both cases.
+• Extend your program to make a graph that shows both, the original velocity 
+curve and the distance curve, in two separate panels of the same figure.
+ Hint: Use plt.subplot(2, 1, j), where j = 1 or 2 for upper/lower panels.
+'''
