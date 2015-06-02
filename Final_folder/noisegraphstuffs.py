e22f382894a8155b9664b58860072e851164208d
dmart030 <dmart030@ucr.edu>
2015-06-02 00:33:02 -0700

    update to the final project

---
+++ Final_folder/noisegraphstuffs.py
@@ -0,0 +1,20 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fftpack import fft
+
+
+DataIN = np.loadtxt('StepSize_0.25_05282015_151046.txt')
+y = DataIN[:]
+x = np.array([i for i in range(len(y))])
+fourier_transform = fft(y)
+plt.ylim(-100,200)
+plt.plot(x,np.imag(fourier_transform), 'b',label = "imaginary fft plot")
+plt.plot(x,np.real(fourier_transform), 'g', label = "real fft plot")
+plt.plot(x,y, 'r', label = "Not fft")
+plt.legend()
+plt.ylabel('Pressure[torr]')
+plt.title('Fourier transform of the noise')
+plt.xlabel('Time[s]')
+plt.show()
